fce248e128ded68a56dc266a83f8453b67710a29
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-11-04 19:48:49 +0100

    Added basic setup.py file including Pelican and Markdown dependencies

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='SkyLines Blog',
+    version='0.0.0',
+    description='',
+    author='',
+    author_email='',
+    packages=[],
+    scripts=[],
+    url='http://blog.skylines-project.org/',
+    license='',
+    install_requires=[
+        "Pelican == 3.3",
+        "Markdown == 2.3.1",
+    ],
+)
